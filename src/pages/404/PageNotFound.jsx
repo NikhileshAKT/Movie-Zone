c1f8ff17376d7904c0cca85966dc733e44c180fb
Nikhilesh Thakur <nikhileshthakur2002@gmail.com>
2024-02-25 21:48:32 +0530

    pagenotfound updated

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -8,8 +8,8 @@ const PageNotFound = () => {
     return (
         <div className="pageNotFound">
             <ContentWrapper>
-                <span className="bigText">404</span>
-                <span className="smallText">Page not found!</span>
+                <span className="bigText">404 </span>
+                <span className="smallText">Page not found !</span>
             </ContentWrapper>
         </div>
     );
